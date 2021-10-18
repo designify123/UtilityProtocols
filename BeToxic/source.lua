a52840e12d3e5580a8b64cbb98bc5636a802f24f
Modularz <79064796+designify123@users.noreply.github.com>
2021-10-18 22:27:13 +0100

    Update source.lua

--- BeToxic/source.lua
+++ BeToxic/source.lua
@@ -1,3 +1,9 @@
 -- WARNING USING THE SOURCE IS NOT RECOMMENDED, IT IS RECOMMENDED TO USE THE SCRIPT.LUA TO RECEIVE AUTO UPDATES FOR BETOXIC.LUA
 
 print("BeToxic Started.")
+
+
+
+local speaker = game.Players.LocalPlayer
+
+local enabled - false 
