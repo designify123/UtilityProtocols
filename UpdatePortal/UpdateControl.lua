d2e172b8b5e5339c4dd408c28cf3b3717a3a0e9a
zov24 <79064796+designify123@users.noreply.github.com>
2021-10-18 18:36:29 +0100

    Update UpdateControl.lua

--- UpdatePortal/UpdateControl.lua
+++ UpdatePortal/UpdateControl.lua
@@ -1,6 +1,6 @@
 
 warn("Update Portal Initalized, finding last gateway...")
-warn("Current Update Portal Version running at build: 1.0.0")
+warn("Current Update Portal Version running at build: 1.0.2")
 
 function FindPortal()
     if point == "BeToxic" then 
