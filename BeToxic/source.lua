fac55f29e20002993b8b169d22084ed2fb524ec6
Modularz <79064796+designify123@users.noreply.github.com>
2021-10-18 22:40:23 +0100

    Update source.lua

--- BeToxic/source.lua
+++ BeToxic/source.lua
@@ -2,8 +2,45 @@
 
 print("BeToxic Started.")
 
+-- Commands
+-- /enable
+-- /disble
+
+ speaker = game.Players.LocalPlayer
+ delay = 1 
+ enabled = false 
+
+local toxicmsgs = { -- U can add mores when wanted
+  "noob",
+  "get good kid"
+}
 
 
-local speaker = game.Players.LocalPlayer
+function msg(str)
+  if str then 
+    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(msg, "All")
+    end
+ end
 
-local enabled - false 
+function init()
+  speaker.Chatted:Connect(function(msg)
+     if msg == "/e disable" and enabled == true then
+        enabled = false 
+        return true
+     end
+      if msg == "/e enable" and enabled == false then
+         enabled = true
+        toxic()
+        return true
+        end
+  end)
+end
+
+function toxic()
+  if not enabled == true then return end
+  repeat 
+    wait(delay)
+    local sp = math.random(1, #toxicmsgs)
+    msg(toxicmsgs[sp])
+    until enabled == false 
+end
