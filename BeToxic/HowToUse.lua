f3dacc7446a92e65c4bdde1f09821a18568f668a
zov24 <79064796+designify123@users.noreply.github.com>
2021-10-18 17:59:38 +0100

    Create HowToUse.lua

---
+++ BeToxic/HowToUse.lua
@@ -0,0 +1,12 @@
+HOW DO I USE THIS SCRIPT? 
+
+In order for this to run, you execute the script and hit the desired key (Default keybind 'X') this can be edited.
+
+HOW TO ADD MORE TO SCRIPT? 
+
+There is a table and add mire stuff to it remember to have table like this
+
+local toxic = {
+     "ha lol noob", -- You need comma for every second last line(s)
+    "get good kid"
+}
