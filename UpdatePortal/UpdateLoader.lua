9023a13df8356e053170b78001f07ba723d9fbd3
Modularz <79064796+designify123@users.noreply.github.com>
2021-10-18 21:08:16 +0100

    Update UpdateLoader.lua

--- UpdatePortal/UpdateLoader.lua
+++ UpdatePortal/UpdateLoader.lua
@@ -1,3 +1,5 @@
 print("Got signal sending..")
 
+warn("Loader responded.. 24:1")
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/designify123/UtilityProtocols/main/UpdatePortal/UpdateControl.lua", true))()
