36d182f543ea79d131e7ccc706bedc8bbefefdbc
Modularz <79064796+designify123@users.noreply.github.com>
2021-10-19 10:34:56 +0100

    Update source.lua

--- RateMyAvatarCustomBooth/source.lua
+++ RateMyAvatarCustomBooth/source.lua
@@ -1,1 +1,16 @@
 warn("RMACB loaded.")
+
+types = "random" -- The types are currently are: random, and order (order is the lapse in order.)
+local speaker = game.Players.LocalPlayer
+local delay = 1
+local isEnabled = true
+
+-- Commands 
+-- /
+
+
+local words = {
+  "put words here",
+  "make sure there is comma at 2nd last line."
+}
+
