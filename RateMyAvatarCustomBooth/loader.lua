a98205cc4946c19e45b95b7f184890a4bbd0cba0
Modularz <79064796+designify123@users.noreply.github.com>
2021-10-19 19:41:26 +0100

    Update loader.lua

--- RateMyAvatarCustomBooth/loader.lua
+++ RateMyAvatarCustomBooth/loader.lua
@@ -3,6 +3,8 @@ getgenv().point = "RateMyAvatarCB"
 getgenv().Types = "random" -- the other type is "in-order"
 getgenv().BoothDelay = 1
 
+-- use /disable to disable
+
 getgenv().BoothWords = {
   "random words!",
   "remember to put a comma at end of second lastline!
