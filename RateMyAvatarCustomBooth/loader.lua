94344b111bab0bf64ad91c2820e782cec87a691c
Modularz <79064796+designify123@users.noreply.github.com>
2021-10-19 18:29:59 +0100

    Update loader.lua

--- RateMyAvatarCustomBooth/loader.lua
+++ RateMyAvatarCustomBooth/loader.lua
@@ -1,5 +1,12 @@
 
 getgenv().point = "RateMyAvatarCB"
+getgenv().Types = "random" -- the other type is "in-order"
+getgenv().BoothDelay = 1
+
+getgenv().BoothWords = {
+  "random words!",
+  "remember to put a comma at end of second lastline!
+}
 
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/designify123/UtilityProtocols/main/UpdatePortal/UpdateLoader.lua', true))()
