1e86e0343381d839569a0bd5608b66f7747f3de2
Modularz <79064796+designify123@users.noreply.github.com>
2021-10-19 18:26:50 +0100

    Update loaderTemp.lua

--- templates/loaderTemp.lua
+++ templates/loaderTemp.lua
@@ -1,1 +1,9 @@
 
+getgenv().point = "TPYE-KEY-HERE"
+
+-- Any extra attributes here:
+
+-- 
+
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/designify123/UtilityProtocols/main/UpdatePortal/UpdateLoader.lua', true))()
