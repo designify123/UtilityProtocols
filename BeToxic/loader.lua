c3caa64ead51c759395716dbde869414607f4c16
Modularz <79064796+designify123@users.noreply.github.com>
2021-10-19 19:37:28 +0100

    Update loader.lua

--- BeToxic/loader.lua
+++ BeToxic/loader.lua
@@ -2,6 +2,11 @@
 
 getgenv().point = "BeToxic"
 
-
+getgenv().delay = 1
+getgenv().toxicmsgs = {
+  "noob",
+  "get good kid",
+  "Add a comma at sencond last line too add nore"
+}
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/designify123/UtilityProtocols/main/UpdatePortal/UpdateLoader.lua', true))()
