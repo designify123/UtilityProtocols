2effe06f4dd310ce9b50da48f92ecd466d0e10d6
Modularz <79064796+designify123@users.noreply.github.com>
2021-10-19 18:33:35 +0100

    Update source.lua

--- RateMyAvatarCustomBooth/source.lua
+++ RateMyAvatarCustomBooth/source.lua
@@ -1,49 +1,47 @@
-warn("RMACB loaded.")
+print("RMACB loaded.")
 
-types = "random" -- The types are currently are: random, and order (order is the lapse in order.)
-local speaker = game.Players.LocalPlayer
-local delay = 1
-local isEnabled = true
+speaker = game.Players.LocalPlayer
+isEnabled = true
 
 -- Commands 
+
+function sendEvent(msg)
+  local args = {
+    [1] = "Update",
+    [2] = {
+        ["DescriptionText"] = msg, 
+        ["ImageId"] = 0
+    }
+}
+    game:GetService("ReplicatedStorage").CustomiseBooth:FireServer(unpack(args))
+end
+
+
 -- /disable
 
 
 
-local words = {
-  "put words here",
-  "make sure there is comma at 2nd last line."
-}
-
 function init()
-  if speaker then 
     speaker.Chatted:Connect(function(msg)
-        if msg not == "/disable" then return end
-        else 
+        if msg  == "/disable" then
         if isEnabled == true then 
           isEnabled = false
+          end
        end
     end)
   end
-end 
 
 function start()
-  if IsEnabled == true then 
-    if types == "random" then
+  if isEnabled == true then 
+    if Types == "random" then
       repeat 
-        wait(delay)
+          wait(BoothDelay)
       local sp = math.random(1, #words)
-        sendEvent(words[sp])
+        sendEvent(BoothWords[sp])
         until IsEnabled == false
       end
    end
 end
 
-function sendEvent(msg)
-  local args = {
-    [1] = ""
-    [2] = {
-     
-}
-    -- Remote goes here
-end
+
+start()
