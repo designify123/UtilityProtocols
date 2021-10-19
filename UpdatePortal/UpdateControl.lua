d4d701b6718cb6a147dc2d3e56f6c5c0114922ea
Modularz <79064796+designify123@users.noreply.github.com>
2021-10-19 11:29:41 +0100

    Update UpdateControl.lua

--- UpdatePortal/UpdateControl.lua
+++ UpdatePortal/UpdateControl.lua
@@ -4,12 +4,16 @@ warn("Current Update Portal Version running at build: 1.0.2")
 
 function FindPortal()
     if point == "BeToxic" then 
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/designify123/UtilityProtocols/main/BeToxic/source.lua', true))()
+       loadstring(game:HttpGet('https://raw.githubusercontent.com/designify123/UtilityProtocols/main/BeToxic/source.lua', true))()
+        warn("BeToxic Client required.")
      end
     if point == "FakeIPLogger" then 
        loadstring(game:HttpGet("https://raw.githubusercontent.com/designify123/UtilityProtocols/main/FakeIPLogger/source.lua", true))()
+        warn("FakeIPLogger Client required.")
+     end 
      if point == "RateMyAvatarCB" then 
-            
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/designify123/UtilityProtocols/main/RateMyAvatarCustomBooth/source.lua", true))()
+        warn("MyAvatarCB-RMACB Client required.")
      end
 end
 
