1315216e005984f4c751877a4d4fd2a5efc43d28
Modularz <79064796+designify123@users.noreply.github.com>
2021-10-20 08:52:51 +0100

    Update main.lua

--- UtilityModules/main.lua
+++ UtilityModules/main.lua
@@ -2,4 +2,26 @@ Players = game:GetService("Players")
 speaker = Players.LocalPlayer
 UM_MOUSE = speaker:GetMouse()
 
-utils = 
+utils = {}
+
+function utils:PlayerToId(target)
+  if Players[target] then
+     return target.UserId
+    end
+end
+
+function utils:IdToPlayer(id)
+  for x, v in pairs(Players:GetPlayer()) do 
+       if v.UserId == id then 
+      return v.Name 
+        end
+    end
+end
+
+function getPos(target)
+  if target == "me" then 
+    return speaker.Character.HumanoidRootPart.CFrame
+  else 
+    return Players[target].Character.HumanoidRootPart.CFrame
+    end
+end
