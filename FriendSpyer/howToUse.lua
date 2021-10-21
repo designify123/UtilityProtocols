8e0ce4c55001c6b15da10401f1701c0eeb79d593
Modularz <79064796+designify123@users.noreply.github.com>
2021-10-21 08:45:30 +0100

    Update howToUse.lua

--- FriendSpyer/howToUse.lua
+++ FriendSpyer/howToUse.lua
@@ -1,1 +1,22 @@
+[[
 
+Hello! 
+if you are wondering how to use friend spyer, please keep in mind your friend (your spying on must be a exploiter.)
+
+Script A: You use this script to unlock other major spying panels in FriendSpyer.
+Script B: is th spyer-main for your friend, without this the friendSpyer or script A cannot operate properly.
+
+We also recommend you to put Script B hidden in obfuscated code, or in a decoy script. (We recommend PSU for obfuscation.)
+Please be award, if script A isnt ativated in a server with script B then you still receive LIMITED panels.
+
+Includes:
+ChatLogger
+GameTempBan
+GameBan
+GameKick
+FriendRequestLog
+snd more..
+
+
+
+]]
