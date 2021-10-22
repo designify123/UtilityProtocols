70815c68d797a68b391ce043438cd9ebe952c2d9
Modularz <79064796+designify123@users.noreply.github.com>
2021-10-22 08:35:29 +0100

    Update loader.lua

--- RateMyAvatarDinoAnimated/loader.lua
+++ RateMyAvatarDinoAnimated/loader.lua
@@ -1,1 +1,9 @@
 
+getgenv().point = "RMADA-Dino"
+
+-- Any extra attributes here:
+getgenv().RMADASpeed = 0.1 -- 0.1 is recomended.
+getgenv.Area = "Booth" -- we also support sign mmode using "Sign" but we will have to alter some of nimation frames
+
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/designify123/UtilityProtocols/main/UpdatePortal/UpdateLoader.lua', true))()
